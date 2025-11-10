9de2bd58555c7921a1246281bdddcea354c68682
JK <justin@kufeldt.com>
2025-11-10 12:02:21 -0500

    Initial (needs testing)

---
+++ src/main/java/me/pintoadmin/pintoRecipes/PintoRecipes.java
@@ -0,0 +1,17 @@
+package me.pintoadmin.pintoRecipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PintoRecipes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/pintoadmin/pintoRecipes/SaveRecipeCommand.java
@@ -0,0 +1,4 @@
+package me.pintoadmin.pintoRecipes;
+
+public class SaveRecipeCommand {
+}
